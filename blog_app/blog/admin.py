8ea07f1da740c0fb74d082fcf71b29930ec8d439
ProgTimes <p.coding063@gmail.com>
2024-07-03 16:03:18 +0200

    some admin fixes

--- blog_app/blog/admin.py
+++ blog_app/blog/admin.py
@@ -1,7 +1,16 @@
-from blog.models import Post
 from django.contrib import admin
+
+from blog.models import Post, Comment
 
 
 @admin.register(Post)
 class BeerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'author',)
+    search_fields = ('title', 'content',)
+    list_filter = ('created_at',)
     prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Comment)
+class BeerAdmin(admin.ModelAdmin):
+    list_display = ('post', 'content', 'author',)
--- blog_app/blog/models.py
+++ blog_app/blog/models.py
@@ -18,3 +18,13 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', kwargs={'slug': self.slug})
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(max_length=350)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
