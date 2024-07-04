e54fb81c01a3c54dfdd05719251ea0b5a1cddb51
ProgTimes <p.coding063@gmail.com>
2024-07-04 15:31:16 +0200

    fix to last commit

--- blog_app/blog/models.py
+++ blog_app/blog/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
