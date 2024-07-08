23a61a86d90a588472fd0c030fda84be120781b8
ProgTimes <p.coding063@gmail.com>
2024-07-08 13:15:09 +0200

    add post editing and deleting

--- blog_app/blog/forms.py
+++ blog_app/blog/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 
+from blog.models import Post
+
 
 class CommentForm(forms.Form):
     comment = forms.CharField(widget=forms.Textarea(attrs={
         'rows': 6, 'maxlength': 350, "class": "form-control", "placeholder": "Enter your comment"
     }), max_length=350, label="")
+
+
+class PostForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Title'}))
+    image = forms.ImageField()
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Content'}))
+
+    class Meta:
+        model = Post
+        fields = ['title', 'image', 'content', 'category']
--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from blog.views import PostListView, PostSingleView, PostListByCategoryView, PostListByAuthorView, AddCommentView
+import blog.views as blog_views
 
 app_name = "blog"
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="post_list"),
-    path("post/<str:slug>", PostSingleView.as_view(), name="post_detail"),
-    path("post/add-comment/<str:post_id>", AddCommentView.as_view(), name="add_comment"),
-    path("category/<str:slug>", PostListByCategoryView.as_view(), name="category"),
-    path("author/<str:slug>", PostListByAuthorView.as_view(), name="author"),
+    path("", blog_views.PostListView.as_view(), name="post_list"),
+    path("post/<str:slug>", blog_views.PostSingleView.as_view(), name="post_detail"),
+    path("post/<str:slug>/edit", blog_views.PostUpdateView.as_view(), name="edit_post"),
+    path("post/<str:slug>/delete", blog_views.delete_post, name="delete_post"),
+    path("post/add-comment/<str:post_id>", blog_views.AddCommentView.as_view(), name="add_comment"),
+    path("category/<str:slug>", blog_views.PostListByCategoryView.as_view(), name="category"),
+    path("author/<str:slug>", blog_views.PostListByAuthorView.as_view(), name="author"),
 ]
